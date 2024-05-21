a8d4b2c5fa65e5daf05be501cb199ef5a67eca47
keitaobbs <keitaobbs@gmail.com>
2024-05-21 21:44:26 +0900

    Fix lkm_sample coding style

--- utils/linux/lkm_sample/lkm_sample.c
+++ utils/linux/lkm_sample/lkm_sample.c
@@ -6,13 +6,15 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 
-static int __init lkm_sample_init(void) {
-  pr_info("Hello, World!\n");
-  return 0;
+static int __init lkm_sample_init(void)
+{
+	pr_info("Hello, World!\n");
+	return 0;
 }
 
-static void __exit lkm_sample_exit(void) {
-  pr_info("Goodbye, World!\n");
+static void __exit lkm_sample_exit(void)
+{
+	pr_info("Goodbye, World!\n");
 }
 
 module_init(lkm_sample_init);
